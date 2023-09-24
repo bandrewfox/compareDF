b09d423f8a1f97f7ee5541f25f4fe182606446ff
Brian Fox <bandrewfox@gmail.com>
2023-09-23 19:40:00 -0700

    added create_sparse_output function and tests

--- tests/testthat/test-fnsOutputs.R
+++ tests/testthat/test-fnsOutputs.R
@@ -185,7 +185,7 @@ new_df = data.frame(var1 = c("A", "B", "C"),
                     val3 = c(1, 2.1, 4)
 )
 
-compare_output = compareDF::compare_df(old_df, new_df, c('var1', 'var2'))
+compare_output = compareDF::compare_df(new_df, old_df, c('var1', 'var2'))
 
 test_that("compare_df: Error out if file name is NULL", {
   expect_error(create_output_table(compare_output, output_type = 'xlsx'), "file_name cannot be null if output format is xlsx")
@@ -215,4 +215,45 @@ test_that("compare_df: Write to file correctly", {
 # )
 # big_output = compareDF::compare_df(old_df, new_df, c('var1', 'var2'))
 # create_output_table(big_output, output_type = 'xlsx', file_name = "test_file.xlsx")
+
+
+#===============================================================================
+# Sparse
+#===============================================================================
+
+context("fnsOutputs: Sparse Output")
+
+old_df = data.frame(var1 = c("A", "B", "C", "C"),
+                    var2 = c("Z", "X", "X", "W"),
+                    val1 = c(1, 2, 3, 4),
+                    val2 = c("A1", "B1", "C1", "C1")
+)
+
+new_df = data.frame(var1 = c("A", "B", "C", "D", "F"),
+                    var2 = c("Z", "X", "X", "X", "X"),
+                    val1 = c(1, 2, 3, 5, 6),
+                    val2 = c("A1", "B1", "C2", "D2", "F2")
+)
+
+compare_output = compareDF::compare_df(new_df, old_df, 'var1')
+
+test_that("compare_df: 1 column in group_col", {
+  out = create_sparse_output(compare_output)
+  expect_true(all(out[1,] == c("C", "value_change", "var2", "X,W", "X")))
+  expect_true(all(out[2,] == c("C", "value_change", "val1", "3,4", "3")))
+  expect_true(all(out[3,] == c("C", "value_change", "val2", "C1,C1", "C2")))
+  expect_true(all(out[4,] == c("D", "row_added", NA, NA, NA), na.rm=T))
+  expect_true(all(out[5,] == c("F", "row_added", NA, NA, NA), na.rm=T))
+  expect_true(all(out[6,] == c("C", "row_deleted", NA, NA, NA), na.rm=T))
+})
+
+compare_output = compareDF::compare_df(new_df, old_df, c('var1', 'var2'))
+
+test_that("compare_df: 2 columns in orig_group_cols", {
+  out = create_sparse_output(compare_output, orig_group_cols = c('var1', 'var2'))
+  expect_true(all(out[1,] == c(3, "C,W", "row_deleted", NA, NA, NA), na.rm=T))
+  expect_true(all(out[2,] == c(4, "C,X", "value_change", "val2", "C1", "C2")))
+  expect_true(all(out[3,] == c(5, "D,X", "row_added", NA, NA, NA), na.rm=T))
+  expect_true(all(out[4,] == c(6, "F,X", "row_added", NA, NA, NA), na.rm=T))
+})
 
